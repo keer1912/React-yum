94865dfddb38701ea65d96455a52753479629b92
Keerthana <2301162@sit.singaporetech.edu.sg>
2025-01-21 12:50:05 +0800

    Allow cors for cross hosting

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const router = express.Router();
+
+router.use(cors());
 
 // Signup route
 router.post('/signup', async (req, res) => {
